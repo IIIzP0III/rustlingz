647e8bb4e1e9b0e11d1f2dd2782ba1579b6f006b
IIIzP0III <zP@solarsystem.coffee>
2023-09-21 23:50:31 -0800

    when you get oxidized

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let data = "Rust is great!".to_string();
 
@@ -22,7 +20,7 @@ fn get_char(data: String) -> char {
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+    let dataz = &data.to_uppercase();
 
-    println!("{}", data);
+    println!("{}", dataz);
 }
